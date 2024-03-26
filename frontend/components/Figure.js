d27a154f588788978933cc5ad02dbf7c876ab406
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-03-26 15:36:21 -0500

    Module 3 Done

--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Figure({ imageURL, caption }) {
     return (
-      <figure>
+      <figure id="F">
         <img src={imageURL}></img>
         <figcaption>Awesome pic taken on {caption}</figcaption>
       </figure>
